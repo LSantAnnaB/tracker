0a9de9b11529208f8666e030f1eeccaaf311a7c2
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2023-08-26 20:06:04 -0300

    Craindo componente "Formulario" e "BarraLateral" e adicionando cronometro

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>
+      <img src="https://api.lorem.space/image" alt="img">
+    </h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: "BarraLateral"
+})
+</script>
+
+<style scoped>
+header {
+ 
+}
+</style>
